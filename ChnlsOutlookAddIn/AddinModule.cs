62fc2ca6bba8668eddec6306a69685ff4c7aca5e
Adam <adam@sevogle.com>
2015-01-13 15:41:09 -0800

    Initial project commit

---
+++ ChnlsOutlookAddIn/AddinModule.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.ComponentModel;
+using System.Windows.Forms;
+using AddinExpress.MSO;
+using Outlook = Microsoft.Office.Interop.Outlook;
+
+namespace ChnlsOutlookAddIn
+{
+    /// <summary>
+    ///   Add-in Express Add-in Module
+    /// </summary>
+    [GuidAttribute("45D69442-87C2-47B7-9AF9-10A2EE0AFDDD"), ProgId("ChnlsOutlookAddIn.AddinModule")]
+    public class AddinModule : AddinExpress.MSO.ADXAddinModule
+    {
+        public AddinModule()
+        {
+            Application.EnableVisualStyles();
+            InitializeComponent();
+            // Please add any initialization code to the AddinInitialize event handler
+        }
+ 
+        #region Component Designer generated code
+        /// <summary>
+        /// Required by designer
+        /// </summary>
+        private System.ComponentModel.IContainer components;
+ 
+        /// <summary>
+        /// Required by designer support - do not modify
+        /// the following method
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            //
+            // AddinModule
+            //
+            this.AddinName = "ChnlsOutlookAddIn";
+            
+            this.SupportedApps = ((AddinExpress.MSO.ADXOfficeHostApp)(
+				AddinExpress.MSO.ADXOfficeHostApp.ohaOutlook 
+				));
+        }
+        #endregion
+ 
+        #region Add-in Express automatic code
+ 
+        // Required by Add-in Express - do not modify
+        // the methods within this region
+ 
+        public override System.ComponentModel.IContainer GetContainer()
+        {
+            if (components == null)
+                components = new System.ComponentModel.Container();
+            return components;
+        }
+ 
+        [ComRegisterFunctionAttribute]
+        public static void AddinRegister(Type t)
+        {
+            AddinExpress.MSO.ADXAddinModule.ADXRegister(t);
+        }
+ 
+        [ComUnregisterFunctionAttribute]
+        public static void AddinUnregister(Type t)
+        {
+            AddinExpress.MSO.ADXAddinModule.ADXUnregister(t);
+        }
+ 
+        public override void UninstallControls()
+        {
+            base.UninstallControls();
+        }
+
+        #endregion
+
+        public static new AddinModule CurrentInstance 
+        {
+            get
+            {
+                return AddinExpress.MSO.ADXAddinModule.CurrentInstance as AddinModule;
+            }
+        }
+
+        public Outlook._Application OutlookApp
+        {
+            get
+            {
+                return (HostApplication as Outlook._Application);
+            }
+        }
+
+    }
+}
+
